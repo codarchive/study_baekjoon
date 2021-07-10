a36bb441458d43813d5ed2f7900767082f78890c
codarchive <jhangww@gmail.com>
2021-07-10 23:19:51 +0900

    Update 0710.py

--- 0710.py
+++ 0710.py
@@ -1,6 +1,6 @@
 #백준 4673(내거)
 def selfnum():
-    gen_set=set() #empty셋 정의할때 {}이렇게 하면 딕셔너리로 타입이 정해져서 이렇게 해야함 ㅡ,ㅡ;
+    gen_set=set() 
     for i in range(1,10001):
         generated_nunber=i+sum(int(num) for num in str(i))
         gen_set= gen_set | {generated_nunber}
